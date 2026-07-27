c65046f5922f1911a50a3d27570486220abc132e
hansmex0929-sudo <hansmex0929@gmail.com>
2026-07-27 16:31:19 -0400

    feat: add geofence administration screen

---
+++ src/app/fleet/geofences/page.tsx
@@ -0,0 +1,16 @@
+import { GeotabScreen } from "@/modules/fleet/integrations/geotab/components/GeotabScreen";
+import { resolveGeotabLocale } from "@/modules/fleet/integrations/geotab/locale";
+import { getGeotabScreen } from "@/modules/fleet/integrations/geotab/screens";
+
+type PageProps = {
+  searchParams?: Promise<{ lang?: string | string[] }>;
+};
+
+export default async function FleetGeofencesPage({ searchParams }: PageProps) {
+  const params = searchParams ? await searchParams : {};
+  const definition = getGeotabScreen(
+    "geofences",
+    resolveGeotabLocale(params.lang),
+  );
+  return <GeotabScreen definition={definition} />;
+}
